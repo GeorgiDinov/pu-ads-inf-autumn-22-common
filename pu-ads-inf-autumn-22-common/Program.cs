9b2ac1093c60e7c1bfa82291a373db7182883cd0
Georgi Dinov <georgidinov1@abv.bg>
2022-10-16 21:46:39 +0300

    Added numericval systems conversions

--- pu-ads-inf-autumn-22-common/Program.cs
+++ pu-ads-inf-autumn-22-common/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            PrintAsciis();
         }// end of method Main
 
         //Common array stuff
@@ -561,6 +562,105 @@
                 return GreatestCommonDivRecursive(a, b);
             }
         }
+
+        //Numerical systems
+        //10 to binary/ binary... to very simple example
+        private static string ToBinaryString(int numberInTenBasis)
+        {
+            String result = "";
+            while (numberInTenBasis > 0)
+            {
+                int reminder = numberInTenBasis % 2;
+                result = reminder + result;// conactinate in front
+                numberInTenBasis /= 2;
+            }
+            return result;
+        }
+
+        private static long ToDecimalNumberFromBinaryString(string binaryString)
+        {
+            int power = binaryString.Length - 1;
+            long result = 0;
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+                string digitStr = binaryString.Substring(i, 1);
+                long digit = long.Parse(digitStr);
+                if (digit != 0)
+                {
+                    result += (long)Math.Pow(2, power);
+                }
+                power--;
+            }
+            return result;
+        }
+
+        //Reminer for how the next example do the work
+        private static void PrintAsciis()
+        {
+            int a = (int)'a';
+            Console.WriteLine("a in ascii = " + a);
+            int z = (int)'z';
+            Console.WriteLine("z in ascii = " + z);
+
+            int capitalA = (int)'A';
+            Console.WriteLine("A in ascii = " + capitalA);
+            int capitalZ = (int)'Z';
+            Console.WriteLine("Z in ascii = " + capitalZ);
+
+            int zero = (int)'0';
+            Console.WriteLine("0 in ascii = " + zero);
+            int nine = (int)'9';
+            Console.WriteLine("9 in ascii = " + nine);
+        }
+
+        //Examples from the guide book
+        //        Преобразуване на число от десетична в p-ична бройна система
+        //За да преобразуваме едно число от десетична бройна система в бройна
+        //система с основа p използваме следния алгоритъм: Започваме да делим
+        //числото на основата p с частно и остатък.Остатъците добавяме в списък.
+        //След това частното става изходното число и продължаваме да делим на p,
+        //докато частното стане равно на 0. Обръщаме списъка с остатъците обратно
+        //и това са цифрите на числото в p-ична бройна система.Когато основата на
+        //новата бройна система е по-голяма от 10, за цифри използваме
+        //последователно големите латински букви, като A = 10, B= 11, C= 12 и т.н.
+        //Буквите получаваме, като към буквата А добавяме остатъкът минус 10 и
+        //получаваме (кода на) съответната буква.За целите числа в десетична
+        //бройна система използваме типа long, а резултатът се записва в списък от
+        //символи.
+        static void Num10toOtherBasis()
+        {
+            long num10;
+            Console.Write("Number: ");
+            num10 = Int64.Parse(Console.ReadLine());
+            int basis;
+            Console.Write("Basis: ");
+            basis = int.Parse(Console.ReadLine());
+            List<char> num_b = new List<char>();
+            long priv, rem;
+            do
+            {
+                priv = num10 / basis;
+                rem = num10 % basis;
+                num_b.Add((char)(rem < 10 ? '0' + rem : 'A' + rem - 10));
+                num10 = priv;
+            }
+            while (num10 > 0);
+            Console.Write("The number in new basis is: ");
+            num_b.Reverse();
+            foreach (char ch in num_b) Console.Write(ch);
+            Console.WriteLine();
+        }
+
+        static long Num10fromOtherBasis(string num_b, int basis)
+        {
+            long num10 = 0;
+            foreach (char ch in num_b)
+                num10 = num10 * basis + (int)(ch >= 'A' ? ch - 'A' + 10 : ch - '0');
+            return num10;
+        }
+
+
+
 
         //Searching
         //Linear search
